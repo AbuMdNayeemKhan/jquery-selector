66b326640fc2dc8bef70f19fe11d944405ad10f0
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-02-16 10:06:50 +0600

    learning jquery

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function magic1(){
+    // $("div").css('background-color', 'orange');
+    // $("#p2").css('font-style', 'italic');
+    // $(".para").css('font-style', 'italic');
+    // $("#div1, #div2, #div3").css('background-color', 'orange');
+    // $("#div1, li").css('background-color', 'pink');
+    // $("div > p").fadeToggle();
+    // $("p:first").fadeToggle();
+    // $("p:even").fadeToggle();
+    //$("p:odd").fadeToggle();
+};
